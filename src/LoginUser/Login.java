bdaa9e65d750c7fb00803465ecc88a86e24e2728
Vega8991 <manuelvega011204@gmail.com>
2025-02-12 10:05:25 +0100

    .

--- src/LoginUser/Login.java
+++ src/LoginUser/Login.java
@@ -1,13 +1,6 @@
 package LoginUser;
-
 import java.util.Scanner;
 
-<<<<<<< HEAD
-public class Login {
-
-    public static void main(String[] args) {
-
-=======
 /**
  * Clase principal que gestiona el proceso de inicio de sesión para dos tipos de usuarios
  * y la ejecución de sus respectivas operaciones.
@@ -20,11 +13,10 @@ public class Login {
     public static void main(String[] args) {
     
         // Creación de instancias de usuarios con credenciales predefinidas.
->>>>>>> dev
         UsuarioTipo1 usuario1 = new UsuarioTipo1("Usuario1", "Contrasena1");
         UsuarioTipo2 usuario2 = new UsuarioTipo2("Usuario2", "Contrasena2");
         Scanner scanner = new Scanner(System.in);
-
+        
         try {
             System.out.print("Introduce nombre del usuario 1 (solo letras y números): ");
             String userName1 = scanner.nextLine();
@@ -39,19 +31,12 @@ public class Login {
         }
 
         try {
-<<<<<<< HEAD
-=======
             // Realiza una operación aritmética del usuario 1.
->>>>>>> dev
             usuario1.operacionAritmetica();
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-<<<<<<< HEAD
-
-=======
         
->>>>>>> dev
         try {
             System.out.print("Introduce nombre del usuario 2 (solo letras y números): ");
             String userName2 = scanner.nextLine();
@@ -61,20 +46,13 @@ public class Login {
             // Verifica las credenciales del usuario 2.
             usuario2.comprobarUsuario(userName2, contrasena2);
             System.out.println("Inicio de sesión exitoso en el usuario 2.");
-<<<<<<< HEAD
-
-=======
             
->>>>>>> dev
         } catch (IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
         try {
-<<<<<<< HEAD
-=======
             // Realiza operaciones con matrices del usuario 2.
->>>>>>> dev
             usuario2.sumarMatrices();
             usuario2.mostrarMatrices();
         } catch (Exception e) {
--- src/LoginUser/UsuarioTipo1.java
+++ src/LoginUser/UsuarioTipo1.java
@@ -1,3 +1,4 @@
+package LoginUser;
 import java.util.Scanner;
 
 /**
