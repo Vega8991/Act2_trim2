b7aaa469e521b7d02fdac341f33c1115f62b1533
Vega8991 <manuelvega011204@gmail.com>
2025-02-12 09:22:04 +0100

    Merge

--- src/LoginUser/Login.java
+++ src/LoginUser/Login.java
@@ -1,7 +1,48 @@
 package LoginUser;
 import java.util.Scanner;
 
+public class Login {   
 
-public class Login {
+    public static void main(String[] args) {
+    	
+        UsuarioTipo1 usuario1 = new UsuarioTipo1("Usuario1", "Contrasena1");
+    	UsuarioTipo2 usuario2 = new UsuarioTipo2("Usuario2", "Contrasena2");
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.print("Introduce nombre del usuario 1 (solo letras y números): ");
+            String userName1 = scanner.nextLine();
+            System.out.print("Introduce la contraseña del usuario 1(mínimo 6 carácteres, debe contener un número): ");
+            String contrasena1 = scanner.nextLine();
+            usuario1.comprobarUsuario(userName1, contrasena1);
+            System.out.println("Inicio de sesión exitoso en usuario 1.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
+        try {
+            usuario1.operacionAritmetica();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        try {
+            System.out.print("Introduce nombre del usuario 2 (solo letras y números): ");
+            String userName2 = scanner.nextLine();
+            System.out.print("Introduce la contraseña del usuario 2(mínimo 6 carácteres, debe contener un número): ");
+            String contrasena2 = scanner.nextLine();
+            usuario2.comprobarUsuario(userName2, contrasena2);
+            System.out.println("Inicio de sesión exitoso en el usuario 2.");
+            
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+            usuario2.sumarMatrices();
+            usuario2.mostrarMatrices();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
